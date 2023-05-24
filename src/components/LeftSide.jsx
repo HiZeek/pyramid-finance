2c0ba6c0c0cc273245dae8930b53e30034873723
Isaac Idibia <idibiaisaac3@gmail.com>
2023-05-24 09:36:50 +0100

    responsive test

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -5,7 +5,7 @@ import RightImg from "../assets/RightImg.svg";
 const LeftSide = () => {
   return (
     <div className="leftSideBg w-2/5 px-10 py-14 hidden h-full lg:flex flex-col justify-between">
-      <p className="text-white font-semibold text-[28px] text-center max-w-[554px]">
+      <p className="text-white font-semibold text-[28px] text-center max-w-[554px] mx-auto">
         Social media shared today, tomorrow or by location
       </p>
       <div className="flex justify-center">
